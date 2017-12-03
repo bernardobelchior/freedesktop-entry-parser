bd805810154be9d406b2f6eab80c58b3cbe4a7df
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-12-03 21:25:31 +0000

    File contents are now read.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,21 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::io::Read;
+use std::fs::File;
 
-pub fn main() {
-    let path = Path::new("/usr/share/applications/");
+pub fn parse_dirs(paths: &'static [&'static str]) {
+    for path in paths {
+        let dirs = Path::new(path).read_dir().expect("read_dir call failed");
 
-    let dirs = path.read_dir().expect("read_dir call failed");
-
-    for entry in dirs {
-        if let Ok(entry) = entry {
-            println!("{:?}", entry.path());
+        for entry in dirs {
+            let entry = entry.unwrap();
+            parse_file(entry.path());
         }
     }
+}
+
+pub fn parse_file(file_path: PathBuf) {
+    let mut file = File::open(file_path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    println!("{}", contents);
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -6,7 +6,9 @@ mod tests {
     use desktop_entry_parser::*;
 
     #[test]
-    fn it_works() {
-        main();
+    fn it_parses_dirs() {
+        let paths : &'static[&'static str] = &["/usr/share/applications/"];
+
+        parse_dirs(paths);
     }
 }
